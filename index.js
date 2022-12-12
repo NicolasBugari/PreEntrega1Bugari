d69c96ffc55fa6c56dbcb0b1d2e94e7e5cbdf41a
NicolasB <nicosm0912@gmail.com>
2022-12-12 17:19:45 -0300

    Primer registro Javascript

--- index.js
+++ index.js
@@ -18,10 +18,84 @@ let ingreso = num1;
 const suma = (a, b) => a + b;
 const interes = (x) => x * 0.0625;
 
-
 let valorPlazoFijo = suma(ingreso, interes(ingreso));
-let mensaje3 = `El proximo mes se acreditara en su cuenta: ${valorPlazoFijo}`;
+let periodo = parseInt(prompt("Ingrese cantidad de meses a invertir del 1 al 12"))
+
+if ( periodo === 1 ) {
+   total = valorPlazoFijo
+ } else if ( periodo === 2 ) {
+   total = valorPlazoFijo * 2
+ } else if ( periodo === 3 ) {
+   total = valorPlazoFijo * 3
+ } else if ( periodo === 4 ) {
+   total = valorPlazoFijo * 4
+ } else if ( periodo === 5 ) {
+   total = valorPlazoFijo * 5
+ } else if ( periodo === 6 ) {
+   total = valorPlazoFijo * 6
+ } else if ( periodo === 7 ) {
+   total = valorPlazoFijo * 7
+ } else if ( periodo === 8 ) {
+   total = valorPlazoFijo * 8
+ } else if ( periodo === 9 ) {
+   total = valorPlazoFijo * 9
+ } else if ( periodo === 10 ) {
+   total = valorPlazoFijo * 10
+ } else if ( periodo === 11 ) {
+   total = valorPlazoFijo * 11
+ } else if ( periodo === 12 ) {
+   total = valorPlazoFijo * 12
+ } else {
+   alert('No seleccionaste ningún tipo de periodo de pagos')
+ }
+
+switch ( periodo ) {
+   case '1':
+      total = valorPlazoFijo
+     break
+   case '2':
+      total = valorPlazoFijo * 2
+     break
+   case '3':
+      total = valorPlazoFijo * 3
+     break
+   case '4':
+      total = valorPlazoFijo * 4
+     break
+   case '5':
+      total = valorPlazoFijo * 5
+     break
+   case '6':
+      total = valorPlazoFijo * 6
+     break
+   case '7':
+      total = valorPlazoFijo * 7
+     break
+   case '8':
+      total = valorPlazoFijo * 8
+     break
+   case '9':
+      total = valorPlazoFijo * 9
+     break
+    case '10':
+      total = valorPlazoFijo * 10
+     break
+   case '11':
+      total = valorPlazoFijo * 11
+     break
+   case '12':
+      total = valorPlazoFijo * 12
+     break
+   default:
+     alert('No seleccionaste ningún periodo de pagos')
+     break;
+}
+
+let mensaje3 = `se acreditara en su cuenta: ${valorPlazoFijo}`;
 alert(mensaje3);
+
+let mensaje4 = `En su cuenta se acreditara el siguiente valor por los meses invertidos: ${periodo}`;
+alert(mensaje4);
 
 let salida = prompt("Desea salir de la aplicacion");
 
@@ -35,7 +109,7 @@ while(salida != "ESC" ){
             break;
        default:
            alert("Por favor elija entre si o no")
-           break;
+           
    }
 }
 
